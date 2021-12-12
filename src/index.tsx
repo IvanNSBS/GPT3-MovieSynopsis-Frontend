24763d71e222395ea2892d8602f4b8895b4344b2
IvanNSBS <insbs@cin.ufpe.br>
2021-12-12 10:06:59 -0300

    Adding gh-pages deploy and removing api key print

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-console.log(process.env)
-console.log("API Key: " + process.env.REACT_APP_COLAB_KEY)
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
